77b5087099b0c57bc855b752bbcecb626c425add
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-04 11:37:31 +0300

    inits project

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -4,7 +4,14 @@
 </script>
 
 <template>
-  
+    <div class="flex mt-2 ml-2 mb-2 bg-white rounded-lg shadow-lg shadow-gray-600">
+      <div class="w-full">
+        <div class="menu-active">
+          <i class="mx-2 my-auto"></i>
+          <label class="cursor-pointer"></label>
+        </div>
+      </div>
+    </div>
 </template>
 
 <style scoped>
