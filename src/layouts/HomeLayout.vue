03640fe9f33207910af5a4af8256fe6fcd56c829
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-04 10:19:15 +0300

    init

---
+++ src/layouts/HomeLayout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const message = ref<string>('Welcome to HomeLayout!');
+</script>
+
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+        <h1 class="text-gray-400 font-semibold bg-white shadow-gray-500 rounded-md text-sm">Hello World!!</h1>
+    </div>
+</template>
