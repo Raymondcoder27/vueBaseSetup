14ed88117ea47f62a8da11cefc3cd7ab4cfa8b23
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-04 10:26:40 +0300

    init setup

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+declare module "vue-router" {
+  interface RouteMeta {
+    parentName?: string
+    requiresAuth: boolean
+  }
+}
+
+const appRoutes: RouteRecordRaw[] = [
+  {
+    path: "/", 
+    name: "home",
+    meta: {requiresAuth:false},
+    component: () => import ("@/layouts/HomeLayout.vue"),
+    redirect: "/dashboard",
+    children: [
+      {
+        path: "/dashboard", 
+        name: "dashboard",
+        meta: {requiresAuth: false},
+        component: ()=> import ("@/domain/dashboard/Dashboard.vue")
+      }
+    ]
+  }
+]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
