300208c039ddb53da5052145a57d75d4621270a8
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-04 11:37:59 +0300

    init setup

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -7,8 +7,8 @@
     <div class="flex mt-2 ml-2 mb-2 bg-white rounded-lg shadow-lg shadow-gray-600">
       <div class="w-full">
         <div class="menu-active">
-          <i class="mx-2 my-auto"></i>
-          <label class="cursor-pointer"></label>
+          <i class="mx-2 my-auto">{{item.icon}}</i>
+          <label class="cursor-pointer">{{item.label}}</label>
         </div>
       </div>
     </div>
