de7534fc86d11ba5701e314e417f59bfaa9db4c7
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-04 10:04:24 +0300

    init

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import logo from "@/assets/images/logo.png"
+</script>
+
+<template>
+  <div class="flex p-2 border-b bg-white border-gray-100 shadow-lg shadow-black-600">
+    <div class="w-full">
+    <div class="flex gap-2">
+      <img :src="logo" class="w-10 my-auto"/>
+      <label class="font-bold font-mono text-xl my-auto mx-1">autoDocs</label>
+    </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
