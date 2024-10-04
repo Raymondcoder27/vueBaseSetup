f3db6d1a6b3518fb84911521f146d1b00af60b15
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-04 10:46:45 +0300

    init

--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
-import { ref } from 'vue';
 import AppNotification from "@/components/AppNotification.vue"
 import AppHeader from '@/components/AppHeader.vue';
 import SideMenu from '@/components/SideMenu.vue';
 </script>
 
 <template>
-    <div>
-        <h1>{{ message }}</h1>
-        <h1 class="text-gray-400 font-semibold bg-white shadow-gray-500 rounded-md text-sm">Hello World!!</h1>
+    <div class="flex flex-col w-screen h-screen bg-white">
+        <div class="flex flex-col relative h-full w-full">
+            <main class="flex flex-col h-full w-full bg-gray-100">
+                <div class="flex flex-row h-full">
+                    <div class="flex flex-col flex-grow rounded p-2">
+                        <div class="relative overflow-y-auto w-full flex-grow"></div>
+                    </div>
+                </div>
+            </main>
+        </div>
     </div>
 </template>
