85f3fd983264dfea45bf8a3c94a8ae7e742df96a
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-04 11:50:03 +0300

    init setup

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -41,7 +41,7 @@
 <template>
     <div class="flex mt-2 ml-2 mb-2 bg-white rounded-lg shadow-lg shadow-gray-600">
       <div class="w-full">
-        <div class="menu-active">
+        <div :class="isRouteActive(item.name) ? 'menu-acive': 'menu'" v-for="(item, idx) in sideMenu" :key="idx" @click="navigate(item.name)">
           <i class="mx-2 my-auto">{{item.icon}}</i>
           <label class="cursor-pointer">{{item.label}}</label>
         </div>
