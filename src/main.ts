ccf3782d70a13ca6f957d70404875863278e2430
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-04 10:11:30 +0300

    init setup

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import "@fortawesome/fontawesome-free/css/all.min.css"
 
 import App from './App.vue'
 import router from './router'
