6af63f734cc7d614d6a64768cadf5d4e276a4e1f
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-04 11:41:47 +0300

    inits project

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -15,5 +15,10 @@
 </template>
 
 <style scoped>
-
+ .menu{
+  @apply w-56 ml-2 flex my-auto text-gray-600 px-2 py-3 hover:text-black-900 cursor-pointer text-sm;
+ }
+ .menu-active{
+  @apply w-52 ml-3 my-auto flex font-bold bg-blue-400 rounded-md text-white px-1 py-2 cursor-pointer text-sm;
+ }
 </style>
