578457095bd381b6d15a4326935dd92b98dfc216
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-04 11:31:42 +0300

    inits project

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
-// import logo from "@/"
+import logo from '@/assets/logo.svg';
 </script>
 
 <template>
-  <div class="text-gray-800 font-semibold text-sm text-center">App Header</div>
+  <div class="flex p-2 border-b bg-white border-gray-100 shadow-lg shadow-black-600">
+    <div class="w-full">
+      <div class="flex gap-2">
+        <img :src="logo" alt="" class="w-10 my-auto">
+        <label for="" class="font-bold font-mono text-xl my-auto mx-1">VueBaseApp</label>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
